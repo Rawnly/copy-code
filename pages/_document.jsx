8044df77b71a792c4ee8d1756bd3ec980793d957
Federico Vitale <fedevitale99@gmail.com>
2019-04-13 23:16:20 +0200

    landing

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -2,26 +2,29 @@ import React from "react";
 import Document, { Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx);
 
-    return { ...initialProps };
-  }
+		return { ...initialProps };
+	}
 
-  render() {
-    return (
-      <html>
-        <Head>
-          <link rel='stylesheet' href='/static/css/zeit/buttons.css' />
-          <link rel='stylesheet' href='/static/css/inputs.css' />
-          <link rel='stylesheet' href='/static/css/master.css' />
-          <link rel='stylesheet' href='/static/themes/dracula.css' />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
-  }
+	render() {
+		return (
+			<html>
+				<Head>
+					<meta charset="UTF-8" />
+					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+					<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+
+					<link rel="stylesheet" href="/static/css/zeit/buttons.css" />
+					<link rel="stylesheet" href="/static/css/inputs.css" />
+					<link rel="stylesheet" href="/static/css/master.css" />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</html>
+		);
+	}
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,109 +1,27 @@
-import React, { useEffect, useState } from "react";
-import Editor from "react-simple-code-editor";
+import React from "react";
+import Link from "next/link";
 
-import hljs from "highlight.js";
-import "highlight.js/styles/tomorrow-night.css";
-
-const isServer = () => typeof window === "undefined";
-
-export default (props) => {
-	const [editorValue, setEditorValue] = useState("");
-	const [isWriting, setIsWriting] = useState(false);
-	const [language, setLanguage] = useState(null);
-	const [orLanguage, setOrLanguage] = useState(null);
-
-	function hightlight(code) {
-		const highlightedCode = hljs.highlightAuto(code);
-
-		if (highlightedCode.second_best) {
-			setOrLanguage(highlightedCode.second_best.language);
-		} else {
-			setLanguage(highlightedCode.language);
-		}
-
-		setTimeout(() => setIsWriting(false), 500);
-
-		return highlightedCode.value;
-	}
-
-	useEffect(() => {
-		if (!isServer()) {
-			const editor = document.querySelector("#editor");
-			editor.setAttribute(
-				"placeholder",
-				`// Just write some code,\n// the language will be automatically detected\n\n// Syntax highlight provided by Highlight.js`,
-			);
-			editor.focus();
-		}
-	}, []);
-
+const Page = () => {
 	return (
-		<section style={{ background: "#111", color: "white" }}>
-			<h3>Just write some Code</h3>
-			<Editor
-				value={editorValue}
-				onValueChange={setEditorValue}
-				highlight={hightlight}
-				padding={25}
-				style={{
-					fontFamily: '"Fira code", "Fira Mono", monospace',
-					fontSize: 18,
-					width: "100%",
-					height: "90%",
-					border: "1px solid #222",
-					borderRadius: "10px",
-					marginBottom: 10,
-				}}
-				textareaId="editor"
-			/>
+		<section style={{ background: "#111", color: "#fff" }}>
 			<div
 				style={{
 					display: "flex",
-					justifyContent: "space-between",
+					justifyContent: "center",
 					alignItems: "center",
-					width: "100%",
+					flexDirection: "column",
+					marginBottom: 25,
 				}}>
-				<small
-					style={{
-						marginRight: "auto",
-						opacity: 0.5,
-						fontWeight: "lighter",
-						alignSelf: "flex-start",
-						marginBottom: "50px",
-						fontSize: 14,
-						width: 250,
-					}}>
-					Detected language:{" "}
-					{language && orLanguage ? (
-						<>
-							{language} - {orLanguage}
-						</>
-					) : (
-						"No Code"
-					)}
-				</small>
-				<small
-					style={{
-						marginLeft: "auto",
-						fontWeight: "lighter",
-						alignSelf: "flex-end",
-						marginBottom: "50px",
-						fontSize: 14,
-						alignSelf: "flex-end",
-					}}>
-					<a
-						style={{
-							color: "#0080ff",
-							fontWeight: "bold",
-						}}
-						href="https://github.com/rawnly/copy-code">
-						Source
-					</a>
+				<h2 style={{ margin: 0 }}>SHARE YOUR CODE WITH EASE</h2>
+				<small>
+					Just <code>copy-paste</code> your file and click the share button
 				</small>
 			</div>
-			<button className="button warning" style={{ alignSelf: "flex-end" }} disabled={isWriting}>
-				<b>SAVE</b>
-			</button>
+			<Link href="/random">
+				<a className="button primary white">Get Started</a>
+			</Link>
 		</section>
 	);
 };
+
+export default Page;
